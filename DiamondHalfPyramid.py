05e2b9d8ee24ff4271be2bc60c7d27c4ab037d2a
deepansh983 <raghudeepansh0@gmail.com>
2025-08-30 17:48:30 +0530

    Add files via upload

---
+++ DiamondHalfPyramid.py
@@ -0,0 +1,20 @@
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+for i in range(4,0,-1):
+    for k in range(0,i):
+        print("*",end=" ")
+    print()            
